6289ef771d0152b9e0d80004a7d235b4b921cc43
MPPL <94387889+MPPL-DEV@users.noreply.github.com>
2022-09-23 11:26:08 -0500

    build files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-element'), 'version' => '7ac4dec2a200fd073c37');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-element'), 'version' => '3e3f7f7290708f48c26b');
